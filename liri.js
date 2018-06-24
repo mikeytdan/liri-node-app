88345203d585354cd8a65f2d097f3288348649db
Michael Daniels <mikeytdan@gmail.com>
2018-06-24 16:08:59 -0600

    Finished the assigment

--- liri.js
+++ liri.js
@@ -4,6 +4,7 @@ var Spotify = require("node-spotify-api");
 var Twitter = require("twitter");
 var inquirer = require("inquirer");
 var request = require("request");
+var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
 var twitter = new Twitter(keys.twitter);
 
@@ -59,33 +60,36 @@ function selectedSpotify() {
             name: "song"
         }
     ]).then(function (response) {
-        var song = response.song;
-        spotify.search({ type: 'track', query: response.song })
-            .then(function (response) {
-                var items = response.tracks.items;
-                for (item of items) {
-                    console.log("------------------------------");
-                    console.log("Song: " + item.name);
-                    var artists = [];
-                    for (artist of item.album.artists) {
-                        artists.push(artist.name);
-                    }
-
-                    if (artists.length > 1) {
-                        console.log("Artists: " + artists.join(", "));
-                    } else {
-                        console.log("Artist: " + artists.shift());
-                    }
-
-                    console.log("Preview: " + item.external_urls.spotify);
-                    console.log("Album: " + item.album.name);
-                    console.log("------------------------------");
-                }
-            })
-            .catch(function (error) {
-                console.log("Error: " + error);
-            });
+        spotifySearchSongName(response.song)
     })
+}
+
+function spotifySearchSongName(songName) {
+    spotify.search({ type: 'track', query: songName })
+        .then(function (response) {
+            var items = response.tracks.items;
+            for (item of items) {
+                console.log("------------------------------");
+                console.log("Song: " + item.name);
+                var artists = [];
+                for (artist of item.album.artists) {
+                    artists.push(artist.name);
+                }
+
+                if (artists.length > 1) {
+                    console.log("Artists: " + artists.join(", "));
+                } else {
+                    console.log("Artist: " + artists.shift());
+                }
+
+                console.log("Preview: " + item.external_urls.spotify);
+                console.log("Album: " + item.album.name);
+                console.log("------------------------------");
+            }
+        })
+        .catch(function (error) {
+            console.log("Error: " + error);
+        });
 }
 
 function selectedGetMovieInfo() {
@@ -99,32 +103,67 @@ function selectedGetMovieInfo() {
         }
     ]).then(function (response) {
         var movieName = response.movie;
-        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-        request(queryUrl, function (error, response, body) {
-            console.log("------------------------------");
-            if (error) {
-                console.log("Error: " + error);
-                selectedGetMovieInfo();
-                return
-            } else if (response.statusCode === 200) {
-                var movie = JSON.parse(body);
-                console.log("Title: " + movie.Title);
-                for (rating of movie.Ratings) {
-                    console.log(`${rating.Source}: ${rating.Value}`);
-                }
-                console.log("Country:: " + movie.Country);
-                console.log("Language: " + movie.Language);
-                console.log("Plot: " + movie.Plot);
-                console.log("Actors: " + movie.Actors);
-            } else {
-                console.log("Error: Something went wrong...");
-                selectedGetMovieInfo();
-            }
-            console.log("------------------------------");
-        });
+        getMovieInfo(movieName);
     })
 }
 
+function getMovieInfo(movieName) {
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    request(queryUrl, function (error, response, body) {
+        console.log("------------------------------");
+        if (error) {
+            console.log("Error: " + error);
+            selectedGetMovieInfo();
+            return
+        } else if (response.statusCode === 200) {
+            var movie = JSON.parse(body);
+            console.log("Title: " + movie.Title);
+            for (rating of movie.Ratings) {
+                console.log(`${rating.Source}: ${rating.Value}`);
+            }
+            console.log("Country:: " + movie.Country);
+            console.log("Language: " + movie.Language);
+            console.log("Plot: " + movie.Plot);
+            console.log("Actors: " + movie.Actors);
+        } else {
+            console.log("Error: Something went wrong...");
+            selectedGetMovieInfo();
+        }
+        console.log("------------------------------");
+    });
+}
+
+function runCommand(command, input) {
+    switch (command) {
+        case "my-tweets":
+            selectedShowMyTweets();
+            break;
+        case "spotify-this-song":
+            spotifySearchSongName(input)
+            break
+        case "movie-this":
+            getMovieInfo(input);
+            break
+        default: break
+    }
+}
+
 function selectedDoWhatItSays() {
-    console.log("Do What It Says");
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+
+        var dataArr = data.split(",");
+        var command = dataArr[0];
+
+        if (dataArr.length == 0) {
+            console.log("Error: No data in random.txt");
+            start();
+        } else if (dataArr.length == 1) {
+            runCommand(dataArr[0], "")
+        } else if (dataArr.length > 1) {
+            runCommand(dataArr[0], dataArr[1])
+        }
+    });
 }
